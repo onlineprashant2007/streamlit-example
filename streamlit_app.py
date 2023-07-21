0c2ddbb3e073edc1f6fd4e6dbec3656f4fc09a84
onlineprashant2007 <140179891+onlineprashant2007@users.noreply.github.com>
2023-07-21 19:36:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,35 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import streamlit.components.v1 as components
 
-"""
-# Welcome to Streamlit!
+# Define your tabs and their content
+tabs = {
+    "Dashboard": "This is the dashboard content.",
+    "Strategies": "Here are the available strategies.",
+    "Services": "Explore our services.",
+    "My Subscriber Reports": "View your subscriber reports.",
+    "Login": "Login to access premium features.",
+}
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Function to create custom tab layout
+def render_tabs():
+    st.write("### Welcome to My App!")
+    st.sidebar.title("Navigation")
+    selected_tab = st.sidebar.radio("", list(tabs.keys()))
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    # Display the selected tab content
+    if selected_tab == "Login":
+        st.write(tabs[selected_tab])
+        # Add your login form or authentication logic here
+    else:
+        st.write(tabs[selected_tab])
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Main app
+def main():
+    st.set_page_config(layout="wide")
+    st.title("My Streamlit Dashboard")
+    
+    # Call the custom tab layout function
+    render_tabs()
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    main()
